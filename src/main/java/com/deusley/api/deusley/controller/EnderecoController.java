8defa3806472ee1b19f6710c445872fa9f53fc1f
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-10-24 00:20:26 -0300

    GitUP

--- src/main/java/com/deusley/api/deusley/controller/EnderecoController.java
+++ src/main/java/com/deusley/api/deusley/controller/EnderecoController.java
@@ -11,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RequestMapping("/api/v1/enderecos")
 @RestController
 @RequiredArgsConstructor
